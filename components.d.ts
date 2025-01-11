6fb2926864ed53844239ac98707fd8ab00eb2d4a
James Ball <jamie.j.ball@gmail.com>
2025-01-10 20:56:40 -0500

    chore add pinia & imports file

--- components.d.ts
+++ components.d.ts
@@ -8,5 +8,14 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     Button: typeof import('primevue/button')['default']
+    Card: typeof import('primevue/card')['default']
+    Contact: typeof import('./src/components/contact/index.vue')['default']
+    Home: typeof import('./src/components/home/index.vue')['default']
+    Image: typeof import('primevue/image')['default']
+    InputText: typeof import('primevue/inputtext')['default']
+    Menubar: typeof import('primevue/menubar')['default']
+    Projects: typeof import('./src/components/projects/index.vue')['default']
+    SelectButton: typeof import('primevue/selectbutton')['default']
+    Textarea: typeof import('primevue/textarea')['default']
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import Aura from '@primevue/themes/aura'
+import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/base.css'
 
+const pinia = createPinia()
 const app = createApp(App)
+
 app.use(PrimeVue, {
   theme: { preset: Aura },
 })
+app.use(pinia)
 app.mount('#app')
