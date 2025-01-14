812027e001d1172e3fd7d3cf50bbe2b6b96c526c
James Ball <jamie.j.ball@gmail.com>
2025-01-14 15:55:16 -0500

    fix: example test

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,11 @@
-import { describe, it, expect } from 'vitest'
+import { describe, expect, it } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import CardFront from '../projects/CardFront.vue'
 
-describe('HelloWorld', () => {
+describe('CardFront', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    const wrapper = mount(CardFront)
     expect(wrapper.text()).toContain('Hello Vitest')
   })
 })
