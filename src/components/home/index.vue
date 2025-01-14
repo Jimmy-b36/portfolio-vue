3ea0d17b4f7daa8594b93a12e1fda75a4ecc4907
James Ball <jamie.j.ball@gmail.com>
2025-01-14 13:41:58 -0500

    feat: add contact card

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -6,22 +6,60 @@
   >
     <h1 class="transform -rotate-90 font-bold text-2xl">Home</h1>
   </Button>
-  <section class="grid grid-cols-7 gap-4 p-4 m-24">
-    <Card class="col-span-7 p-5 bg-dark-500 rounded-lg" :unstyled="true">
+  <section class="grid grid-cols-7 gap-8 p-4 m-24">
+    <Card class="col-span-7 p-5 bg-dark-400 rounded-lg" :unstyled="true">
       <template #title>
         <h1 class="text-4xl font-bold text-center text-highlight">James Ball</h1>
         <h2 class="text-lg text-center text-zinc-400">Full Stack Developer</h2>
       </template>
     </Card>
 
-    <Card class="col-span-3 !bg-dark-500">
-      <template #content>
-        <Image
-          src="https://github.com/Jimmy-b36/portfolio/blob/main/public/images/ProfilePic.jpg?raw=true"
-          alt="Profile picture"
-        />
-      </template>
-    </Card>
+    <div class="col-span-3 grid gap-4">
+      <Card class="!bg-dark-400 flex justify-center items-center">
+        <template #content>
+          <Image :src="profilePic" alt="Profile picture" />
+        </template>
+      </Card>
+
+      <Card class="!bg-dark-400">
+        <template #title>
+          <h2 class="text-2xl text-center text-highlight font-bold">Contact</h2>
+        </template>
+        <template #content>
+          <div class="flex flex-col gap-2 items-center">
+            <p class="text-zinc-200">
+              <a
+                @click="copyToClipboard('jamie.j.ball@gmail.com')"
+                class="cursor-pointer"
+                v-if="copied === null"
+                >jamie.j.ball@gmail.com</a
+              >
+              <span v-if="copied" class="text-green-400">Copied!</span>
+              <span v-if="!copied && copied !== null" class="text-red-400">Copy-failed</span>
+            </p>
+            <div class="flex flex-row">
+              <i
+                ><a
+                  href="https://www.github.com/Jimmy-b36"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  ><img :src="gitSvg" alt="github logo" class="h-7 w-7 m-2 cursor-pointer" /></a
+              ></i>
+              <i
+                ><a
+                  href="https://www.linkedin.com/in/james-ball-57831a229/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  ><img
+                    :src="linkedInSvg"
+                    alt="linkedin logo"
+                    class="h-7 w-7 m-2 cursor-pointer" /></a
+              ></i>
+            </div>
+          </div>
+        </template>
+      </Card>
+    </div>
 
     <div class="col-span-4 grid gap-4">
       <Card class="!bg-dark-400">
@@ -36,7 +74,7 @@
         </template>
       </Card>
 
-      <Card class="!bg-dark-500">
+      <Card class="!bg-dark-400">
         <template #title>
           <h2 class="text-2xl text-center text-highlight font-bold">Education</h2>
         </template>
@@ -47,7 +85,7 @@
         </template>
       </Card>
 
-      <Card class="!bg-dark-300">
+      <Card class="!bg-dark-400">
         <template #title>
           <h2 class="text-2xl text-center text-highlight font-bold">Technologies</h2>
         </template>
@@ -64,8 +102,28 @@
 </template>
 
 <script setup lang="ts">
-import { icons } from './iconImports'
+import gitSvg from '@/assets/icons/github-dark.svg?url'
+import linkedInSvg from '@/assets/icons/linkedin.svg?url'
+import profilePic from '@/assets/images/profile-pic.png?url'
+
+import { icons } from '@/utils/iconImports'
+import { ref } from 'vue'
 const props = defineProps<{ command: () => void }>()
+
+const copied = ref<boolean | null>(null)
+
+const copyToClipboard = (text: string) => {
+  try {
+    navigator.clipboard.writeText(text)
+    copied.value = true
+  } catch (error) {
+    copied.value = false
+  } finally {
+    setTimeout(() => {
+      copied.value = null
+    }, 2000)
+  }
+}
 </script>
 
 <style lang="css"></style>
